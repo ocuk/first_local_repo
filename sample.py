768913e408532045899c881157c67707c87e2636
ocuk <overkholyak@gmail.com>
2017-08-06 21:38:42 +0300

    mabrook, first commit kkk

---
+++ sample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import cv2
+import numpy 
+
+def print_it_out(content):
+	print(content)
+
+string = 'first atempt to write a good python program - check!'
+
+if __name__ == "__main__":
+	print_it_out(string) 
